bbb3d1f0cb7b4fce09973e35a41e50f6bd429791
Admin <Admin@DESKTOP-5T4IDV3>
2020-09-03 21:24:56 +0530

    server code

--- netty-multipleClients/src/main/java/com/examples/netty_multipleClients/MyChatServerHandler.java
+++ netty-multipleClients/src/main/java/com/examples/netty_multipleClients/MyChatServerHandler.java
@@ -7,21 +7,14 @@ import io.netty.channel.group.ChannelGroup;
 import io.netty.channel.group.DefaultChannelGroup;
 import io.netty.util.concurrent.GlobalEventExecutor;
 
-/**
- * Implementation of message broadcast Server starts and then there will be
- * multiple users to establish a connection with the server and the server will
- * print out *** is online and will print out *** on other clients If the client
- * disconnects from the server, it will output *** offline in the console, while
- * broadcasting other clients *** has been offline
- */
 public class MyChatServerHandler extends SimpleChannelInboundHandler<String> {
 
-	// channelGroup: saves all channel objects of the client linked to the server
+	
 	private static ChannelGroup channelGroup = new DefaultChannelGroup(GlobalEventExecutor.INSTANCE);
 
 	protected void channelRead0(ChannelHandlerContext ctx, String msg) throws Exception {
 		Channel channel = ctx.channel(); // Channel object that sends the message
-		// Pick yourself out
+	
 		System.out.println("["+channel.remoteAddress() +"]--> request received from Client : "+msg);
 		channelGroup.forEach(ch -> {
 			if (channel != ch) { // not yourself
